163ae583c4f4095135007cb244fdd3e58e3f39eb
Danieltambun12 <56190893+Danieltambun12@users.noreply.github.com>
2019-10-25 18:32:41 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,11 @@
+#ini variable & nilai
+
+namalengkap = "Daniel tambun"
+namapanggilan = "Daniel"
+npm = "311910490"
+tempatlahir = "Bandung"
+umur ="19"
+telf = "0813 8297 8794"
+alamat = "Cibitung"
+
+print("selamat siang, \n Let me introduce my self. My name is",namalengkap, ", but you can call me",namapanggilan, ", My npm is",npm,",I was born",tempatlahir,", and i am",umur,",year old , I am very glad if you want to invite my house in", alamat,", so , don't forget to call me before with the number",telf,"\n thank you")
